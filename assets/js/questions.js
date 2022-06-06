e8bc844b2feb1a315acae4547a2fe5e128919cc0
Sandy <sandycchen30@gmail.com>
2022-06-05 18:08:53 -0700

    added js

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+var questions = [
+  {
+    title: "Javascript is an _______ language?",
+    choices: ["procedural", "Object-Based", "Object-Oriented", "none of the above"],
+    answer: "Object-Oriented"
+  },
+  {
+    title: "Which of the following keywords is used to define a variable in Javascript?",
+    choices: ["let", "var", "Both A and B", "none of the above"],
+    answer: "Both A and B"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
+];
